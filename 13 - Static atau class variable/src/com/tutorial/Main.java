7cbdcfe341dfba8ef360c5eb1b67447ef60f403b
paul novrandes <novrandes.tanuma@gmail.com>
2022-12-07 17:12:42 +0700

    Add files via upload

---
+++ 13 - Static atau class variable/src/com/tutorial/Main.java
@@ -0,0 +1,43 @@
+package com.tutorial;
+
+class Display {
+    static String type = "Display";
+    private String name;
+
+    Display(String name) {
+        this.name = name;
+    }
+
+    void setType(String typeInput) {
+        // type = typeInput; // alternatif 1
+        // this.type = typeInput; // alternatif 2
+        Display.type = typeInput; // alternatif 3
+    }
+
+    void show() {
+        System.out.println("Display Name = " + this.name);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Display display1 = new Display("Monitor");
+        display1.show();
+
+        Display display2 = new Display("Smartphone");
+        display2.show();
+
+        // tampilkan static variabel atau class variabel
+
+        // kita coba mengganti variable static
+        // Display.type = "Spanduk";
+        display1.setType("Monitor");
+
+        System.out.println("\nMenampilkan static variabel");
+        System.out.println(display1.type);
+        System.out.println(display2.type);
+        System.out.println(Display.type);
+
+    }
+}
