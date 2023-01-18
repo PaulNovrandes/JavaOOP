22bd38e982c8c3988236ac372fb34ce64a470382
PAUL NOVRANDES TANUMA <novrandes.tanuma@gmail.com>
2023-01-18 14:40:34 +0700

    Update HeroStrenght.java

--- 19 Overriding Methods/src/com/tutorial/HeroStrenght.java
+++ 19 Overriding Methods/src/com/tutorial/HeroStrenght.java
@@ -4,7 +4,9 @@ public class HeroStrenght extends Hero{
     double health;
 
     /*  overriding methode -> digunakan untuk menimpa atau memodivikasi
-        methode yang diinherit atau diturunkan dari super classnya.    
+        methode yang diinherit atau diturunkan dari super classnya. contohnya pada methode display dibawah ini, kita dapat menimpa
+        isi dari methode display yang sudah ada dengan isian methode yang baru. sehingga pada sub class berikut saat dijalankan tidak
+        mengikuti dari superclassnya.
     */
 
     void display(){
